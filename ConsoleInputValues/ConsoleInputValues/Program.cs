b3112d8d78a5840d2a403a6e5e4689b40bd302dd
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-06 17:59:34 -0800

    Operator Application

---
+++ ConsoleInputValues/ConsoleInputValues/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Think of a number, any number. What is it?");
+
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        double product = num1 * 50;
+        Console.WriteLine(num1 + " multiplied by 50 = " + product);
+        //Console.ReadLine();
+        
+
+        Console.WriteLine("Think of a new number. What is it?");
+
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double sum = num2 + 25;
+        Console.WriteLine(num2 + " + 25 = " + sum);
+        //Console.ReadLine();
+
+        Console.WriteLine("Give me another number!");
+
+        double num3 = Convert.ToDouble(Console.ReadLine());
+        double quotDec = (double)num3 / 12.5;
+        Console.WriteLine(num3 + " divided by 12.5 = " + quotDec);
+        //Console.ReadLine();
+
+        Console.WriteLine("Another number, please!");
+
+        double num4 = Convert.ToDouble(Console.ReadLine());
+        double fifty = 50;
+        bool lgrFifty = num4 > fifty;
+        Console.WriteLine("Is " + num4 + " greater than 50? " + lgrFifty);
+        //Console.ReadLine();
+
+        Console.WriteLine("One last number!");
+
+        double num5 = Convert.ToDouble(Console.ReadLine());
+        double remainder = num5 % 7;
+        Console.WriteLine("The remainder of " + num5 + " divided by 7 is " + remainder);
+        Console.ReadLine();
+
+
+         
+         }
+}
---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus five = " + difference);
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 17;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine(); //this rounds to 5
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine(); //even number.
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine(); //odd number.
+
+            //COMPARE OPERATORS
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemp = 70;
+            //int currentTemp = 72;
+
+            //bool isWarm = currentTemp > roomTemp;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            int roomTemp = 70;
+            int currentTemp = 70;
+
+            //bool isWarm = currentTemp >= roomTemp;
+            bool isWarm = currentTemp != roomTemp; //!= means 'not equal'
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+        }
+    }
+}
