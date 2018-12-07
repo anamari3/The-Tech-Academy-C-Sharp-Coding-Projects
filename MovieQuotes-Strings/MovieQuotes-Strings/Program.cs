6a4db02955a83a767cbc2b0918d971641e6ceb31
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-07 15:31:35 -0800

    Strings Drill

--- MovieQuotes-Strings/MovieQuotes-Strings/Program.cs
+++ MovieQuotes-Strings/MovieQuotes-Strings/Program.cs
@@ -1,22 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace StringsDrill
+{
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-        string actorMain = "Arnold Schwarzenegger";
-        string actorSupport = "Robert Conrad";
-        string charMain = "Howard Langston";
-        string charSupport = "Officer Hummell";
 
-        Console.WriteLine("Jingle All the Way");
-        Console.WriteLine(charMain + " played by " + actorMain + " and " + charSupport + " played by " + actorSupport);
+            string actorMain = "Arnold Schwarzenegger";
+            string actorSupport = "Robert Conrad";
+            string charMain = "Howard Langston";
+            string charSupport = "Officer Hummell";
 
-        charMain = charMain.ToUpper();
-        charSupport = charSupport.ToUpper();
+            Console.WriteLine("Jingle All the Way");
+            Console.WriteLine(charMain + " played by " + actorMain + " and " + charSupport + " played by " + actorSupport);
 
-        Console.WriteLine(charSupport + ": Turbo Man, we could use a man like you on the force. \n \n" + charMain + ": [In Turbo Man outfit] Thanks, I'll keep that in mind. \n Oh, I'm sorry about the bike, and the coffee, and the bus, and...the bomb. ");
-        Console.ReadLine();
+            charMain = charMain.ToUpper();
+            charSupport = charSupport.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(charSupport + ": Turbo Man, we could use a man like you on the force. \n \n" + charMain + ": [In Turbo Man outfit] Thanks, I'll keep that in mind. \n Oh, I'm sorry about the bike, and the coffee, and the bus, and...the bomb. ");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
         }
     }
-
+}
