ca0ca7de266c422885787931b3f36d3508096cab
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-07 12:53:42 -0800

    initial boolean compare code and loop tutorial

---
+++ FavoiteSeason-BooleanCompare/FavoiteSeason-BooleanCompare/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Gues which season is my favorite!");
+        Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
+
+        string guessSeason = Console.ReadLine();
+        bool isGuessed = guessSeason == Fall;
+
+        while (!isGuessed)
+        {
+            switch (guessSeason)
+            {
+                case Winter:
+                    Console.WriteLine("Nope! Too rainy. Guess again!");
+                    Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
+                    guessSeason = Console.ReadLine();
+                    break;
+                case Summer:
+                    Console.WriteLine("Sunny! But no.");
+                    Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
+                    guessSeason = Console.ReadLine();
+                    break;
+                case Spring:
+                    Console.WriteLine("Not Spring. Guess again.");
+                    Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
+                    guessSeason = Console.ReadLine();
+                    break;
+                case Fall:
+                    Console.WriteLine("Yes! I love Fall!");
+                    isGuessed = true;
+                    break;
+                default:
+                    Console.WriteLine("No.");
+                    break;
+            }
+
+        }
+    }
+    }
+
