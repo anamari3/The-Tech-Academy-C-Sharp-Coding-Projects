1116a23b9a0504451ab93212a39a73dfe8e70536
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-07 14:16:29 -0800

    Fav Season finalized

--- FavoiteSeason-BooleanCompare/FavoiteSeason-BooleanCompare/Program.cs
+++ FavoiteSeason-BooleanCompare/FavoiteSeason-BooleanCompare/Program.cs
@@ -4,32 +4,29 @@
     {
         static void Main()
         {
-        Console.WriteLine("Gues which season is my favorite!");
-        Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
+        Console.WriteLine("Guess which season is my favorite!");
+        Console.WriteLine("Type 'A' for Winter, 'B' for Summer, 'C' for Spring, or 'D' for Fall?");
 
-        string guessSeason = Console.ReadLine();
-        bool isGuessed = guessSeason == Fall;
+        char guessSeason = Convert.ToChar(Console.ReadLine());
+        bool isGuessed = guessSeason == 'D';
 
         while (!isGuessed)
         {
             switch (guessSeason)
             {
-                case Winter:
+                case 'A':
                     Console.WriteLine("Nope! Too rainy. Guess again!");
-                    Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
-                    guessSeason = Console.ReadLine();
-                    break;
-                case Summer:
+                    Console.WriteLine("Type 'A' for Winter, 'B' for Summer, 'C' for Spring, or 'D' for Fall?");
+                    guessSeason = Convert.ToChar(Console.ReadLine()); break;
+                case 'B':
                     Console.WriteLine("Sunny! But no.");
-                    Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
-                    guessSeason = Console.ReadLine();
-                    break;
-                case Spring:
+                    Console.WriteLine("Type 'A' for Winter, 'B' for Summer, 'C' for Spring, or 'D' for Fall?");
+                    guessSeason = Convert.ToChar(Console.ReadLine()); break;
+                case 'C':
                     Console.WriteLine("Not Spring. Guess again.");
-                    Console.WriteLine("Choose one: Winter, Summer, Spring, or Fall?");
-                    guessSeason = Console.ReadLine();
-                    break;
-                case Fall:
+                    Console.WriteLine("Type 'A' for Winter, 'B' for Summer, 'C' for Spring, or 'D' for Fall?");
+                    guessSeason = Convert.ToChar(Console.ReadLine()); break;
+                case 'D':
                     Console.WriteLine("Yes! I love Fall!");
                     isGuessed = true;
                     break;
@@ -39,6 +36,37 @@
             }
 
         }
+
+        Console.WriteLine("Guess which is the reason I love Fall the most.");
+        Console.WriteLine(" Type 1 for Halloween, 2 for Pumpkin Spice Lattes, or 3 for colored leaves");
+        int reason = Convert.ToInt32(Console.ReadLine());
+        bool isPicked = reason == 1;
+
+        do
+        {
+            switch (reason)
+            {
+                case 1:
+                    Console.WriteLine("Yes! I go big for Halloween!");
+                    isPicked = true;
+                    break;
+                case 2:
+                    Console.WriteLine("They're a a good reason to love Fall, but not my favorite");
+                    Console.WriteLine(" Type 1 for Halloween, 2 for Pumpkin Spice Lattes, or 3 for colored leaves");
+                    reason = Convert.ToInt32(Console.ReadLine()); break;
+
+                case 3:
+                    Console.WriteLine("Those are nice, but not the best");
+                    Console.WriteLine(" Type 1 for Halloween, 2 for Pumpkin Spice Lattes, or 3 for colored leaves");
+                    reason = Convert.ToInt32(Console.ReadLine()); break;
+                default:
+                    Console.WriteLine("No.");
+                    break;
+            }
+        }
+        while (!isPicked);
+        Console.ReadLine();
+
     }
-    }
+}
 
