77ff57c3215b417a8e2d2f6b09e79c64c961885f
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-06 21:43:05 -0800

    Car Insurance Approval drill & Boolean logic tutorial

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false); //AND operator
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false); //OR operator
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false); //true operator
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false); //not operator
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false); //reads true if one of these are true but not both
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Have you ever had a DUI?");
+        string x = Console.ReadLine();
+            bool dui = new bool();
+            if (x == "yes" || x == "y" || x == "yeah")
+            {
+                dui = true;
+            }
+            else
+            {
+                dui = false;
+            }
+
+        Console.WriteLine("How many speeding tickets do you have?");
+
+        int tickets = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Qualified?");
+        bool agequalified = Convert.ToBoolean(age > 15);
+        bool duiqualified = dui == false;
+        bool ticketsqualified = Convert.ToBoolean(tickets <= 3);
+
+        Console.WriteLine(agequalified && duiqualified && ticketsqualified);
+        Console.ReadLine();
+        }
+    }
+
