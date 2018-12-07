046e861c01422f5d2ba46439bbdb4207b7d2f839
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-07 15:21:41 -0800

    String Drill and tutorial

---
+++ MovieQuotes-Strings/MovieQuotes-Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        string actorMain = "Arnold Schwarzenegger";
+        string actorSupport = "Robert Conrad";
+        string charMain = "Howard Langston";
+        string charSupport = "Officer Hummell";
+
+        Console.WriteLine("Jingle All the Way");
+        Console.WriteLine(charMain + " played by " + actorMain + " and " + charSupport + " played by " + actorSupport);
+
+        charMain = charMain.ToUpper();
+        charSupport = charSupport.ToUpper();
+
+        Console.WriteLine(charSupport + ": Turbo Man, we could use a man like you on the force. \n \n" + charMain + ": [In Turbo Man outfit] Thanks, I'll keep that in mind. \n Oh, I'm sorry about the bike, and the coffee, and the bus, and...the bomb. ");
+        Console.ReadLine();
+        }
+    }
+
---
+++ StringFunctions-Tutorial/StringFunctions-Tutorial/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Ana";
+            //string quote = "The man said, \"Hello\", Ana. \n Hello on a new line. \n \t Hello on a tab \\Hello";
+            ////string fileName = "C:\\Users\\Ana";
+            //string fileName = @"C:\Users\Ana";
+
+            //bool trueOrFale = name.Contains("s");
+            //trueOrFale = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            ////name = name.ToUpper();
+            //name = name.ToLower();
+
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            //string name = "Ana";
+            //name = "Joe";
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Ana");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
