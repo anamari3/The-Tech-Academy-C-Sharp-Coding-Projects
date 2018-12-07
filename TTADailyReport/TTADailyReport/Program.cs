58c99b76173074dcac6f2c25db68e728bd9133f7
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-06 16:19:53 -0800

    Adding Drill TTADaily Report, and Vaiable and Data Types tutorial

---
+++ TTADailyReport/TTADailyReport/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+        Console.WriteLine("What course are you on?");
+
+        string courseName = Console.ReadLine();
+
+        Console.WriteLine("What page number within " + courseName + " are you on?");
+
+        string coursePage = Console.ReadLine();
+
+        Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+
+        bool courseHelp = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+
+        string posExp = Console.ReadLine();
+
+        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+
+        string feedback = Console.ReadLine();
+
+        Console.WriteLine("How many hours did you study today?");
+
+        int hours = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+    }
+}
