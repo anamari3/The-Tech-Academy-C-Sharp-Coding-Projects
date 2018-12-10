2d4432bf43931cc5da5b70fc2c852ac6b0a8e2bf
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-10 14:15:46 -0800

    Try/Catch Drill

--- TryCatch-Drill/TryCatch-Drill/Program.cs
+++ TryCatch-Drill/TryCatch-Drill/Program.cs
@@ -9,17 +9,32 @@ namespace TryCatch_Drill
     class Program
     {
         static void Main(string[] args)
-        {
-            List<int> numbers = new List<int> { 10, 22, 34, 46, 58, 60, 72, 84, 96 };
-            Console.WriteLine("Pick a number between 1 and 100");
-            int pick = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < numbers.Count; i++) ; //THIS IS WRONG
-            if (pick > 1)
+        { 
+            try
             {
-                int quotient = (numbers[i] / pick);
-                //Console.WriteLine(numbers[i] + " divided by " + pick);
+                List<int> numbers = new List<int> { 10, 22, 34, 46, 58, 60, 72, 84, 96 };
+                List<int> picks = new List<int>();
+                Console.WriteLine("Pick a number between 1 and 100");
+                int pick = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in numbers)
+                {
+                    picks.Add(number / pick);
+                }
+                foreach (int added in picks)
+                {
+                    Console.WriteLine("My number divided by " + pick + " = " + added);
+                }
             }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please use a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Don't divide by zero");
+            }
+            Console.WriteLine("The program has emerged from the try/catch block and continued on with program execution");
             Console.ReadLine();
         }
     }
