bb823b21a556bf0bef0226a8f5c88b20dbe816bd
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-10 16:15:43 -0800

    Classes-Drill draft

---
+++ Classes-Drill/Classes-Drill/Integer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Drill
+{
+    public class Integer
+    {
+        //public Integer()
+        //{
+            public Integer()
+        { 
+            int addTwoNumbers(int number1, int number2)
+            {
+                return number1 + number2;
+            }
+        }
+        public int squared(int number3)
+            {
+                return number3 * number3;
+            }
+
+           public int multiply(int number4, int number5)
+            {
+                return number4 * number5;
+            }
+            //public int addTwoNumbers { get; set; }
+            //public int squared { get; set; }
+            //public int multiply { get; set; }
+     }
+    
+}
---
+++ Classes-Drill/Classes-Drill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number between 1 and 10");
+            int pick = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        
+    }
+}
