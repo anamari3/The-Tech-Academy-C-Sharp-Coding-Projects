22996d8a6adba60a8327dbefd4fc909f49f333e8
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-11 16:20:49 -0800

    Method Calling Practice Drill #3

---
+++ MethodCalling-Drill/MethodCalling-Drill/Operation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodCalling_Drill
+{
+    public class Operation
+    {
+        public int Power(int num1)
+        {
+            int result = num1 * num1;
+            Console.WriteLine(num1 + " to the second power is " + result);
+            return result;
+
+        }
+
+        public decimal Divide (decimal num2)
+        {
+            int new2 = decimal.ToInt32(num2);
+            int result = new2 / 3;
+            Console.WriteLine(num2 + " divided by 3 equals " + result);
+            return result;
+        }
+
+        public string Addition (string num3)
+        {
+            int new3 = Convert.ToInt32(num3);
+            int result = new3 + 20;
+            string stringResult = Convert.ToString(result);
+            Console.WriteLine(new3 + " plus 20 equals " + result);
+            return stringResult;
+        }
+    }
+}
---
+++ MethodCalling-Drill/MethodCalling-Drill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodCalling_Drill
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Operation operation = new Operation(); //instantiate
+            operation.Power(12);
+
+            operation.Divide(100.20m);
+
+            operation.Addition("15");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
