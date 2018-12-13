abb424bb861422aae2eea3d5322892f5be45f60c
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-13 13:40:30 -0800

    drill 131

---
+++ Gernerics-131Drill/Gernerics-131Drill/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gernerics_131Drill
+{
+    public class Employee<T> : Person
+    {
+        //int id { get; set; }
+        public List<T> things { get; set; }
+
+        public override void SayName()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+        //public void Quit()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public static Employee<T> operator ==(Employee<T> employee, Employee<T> id)
+        //{
+        //    employee.id.Equals(id);
+        //    return employee;
+        //}
+
+        //public static Employee<T> operator !=(Employee<T> employee, Employee<T> id)
+        //{
+        //    employee.id.CompareTo(id);
+        //    return employee;
+        //}
+
+
+    }
+}
---
+++ Gernerics-131Drill/Gernerics-131Drill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gernerics_131Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string>() { "pen", "paper", "keyboard", "monitor" };
+
+            Employee<int> employ = new Employee<int>();
+            employ.things = new List<int>() { 2, 4, 6, 8, 10 };
+
+            System.Collections.IList list = employee.things;
+            for (int i = 0; i < list.Count; i++)
+            {
+                Employee<int> things = (Employee<int>)list[i];
+                Console.WriteLine(things);
+            }
+            //Class_124Drill.IQuittable employee = new Class_124Drill.Employee();
+            ////employee<T> employee = new Employee<T>();
+            ////employee.firstName = "Sample";
+            ////employee.lastName = "Student";
+            ////employee.SayName();
+            //employee.Quit();
+        }
+    }
+}
