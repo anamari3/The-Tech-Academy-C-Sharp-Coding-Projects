a61d25eb1ea4afd1f9638dbe6582aea968b75bb4
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-12 20:06:18 -0800

    drill 124 - creating abstract class

--- TwentyOne-Tutorial/TwentyOne-Tutorial/Program.cs
+++ TwentyOne-Tutorial/TwentyOne-Tutorial/Program.cs
@@ -10,57 +10,20 @@ namespace TwentyOne_Tutorial
     {
         static void Main(string[] args)
         {
-                                                    //TwentyOneGame game = new TwentyOneGame(); //DEMONSTRATING INHERETENCE
-                                                    //game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
-                                                    //game.ListPlayers();
-                                                    //Console.ReadLine();
-                                                    //game.Play();
-
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-            //                                        //deck.Cards = new List<Card>(); //REMOVING TO CREATE CONSTRUCTOR
-
-            //                                        //Card cardOne = new Card();
-            //                                        //cardOne.Face = "Queen";
-            //                                        //cardOne.Suit = "Spades";
-
-            //                                        //deck.Cards.Add(cardOne);
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
+            game.ListPlayers();
             Console.ReadLine();
+
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
         }
-
-                                                    //public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-                                                    //{
-                                                    //    timesShuffled = 0;
-                                                    //    for (int i = 0; i < times; i++)
-                                                    //    {
-                                                    //        timesShuffled++;
-                                                    //        List<Card> TempList = new List<Card>();
-                                                    //        Random random = new Random();
-
-                                                    //        while (deck.Cards.Count > 0)
-                                                    //        {
-                                                    //            int randomIndex = random.Next(0, deck.Cards.Count);
-                                                    //            TempList.Add(deck.Cards[randomIndex]);
-                                                    //            deck.Cards.RemoveAt(randomIndex);
-                                                    //        }
-                                                    //        deck.Cards = TempList;
-                                                    //    }
-                                                    //    return deck;
-                                                    //}
-
-                                                    //public static Deck Shuffle(Deck deck, int times)
-                                                    //{
-                                                    //    for (int i = 0; i < times; i++)
-                                                    //    {
-                                                    //        deck = Shuffle(deck);
-                                                    //    }
-                                                    //    return deck;
-                                                    //}
     }
 }
