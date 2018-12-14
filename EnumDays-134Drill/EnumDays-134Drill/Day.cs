d72cd34c25770ba2f1c0ec1a602c50741105a6d7
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-13 16:24:40 -0800

    134 Drill Enum

---
+++ EnumDays-134Drill/EnumDays-134Drill/Day.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDays_134Drill
+{
+    public class Day
+    {
+        public enum DayOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
--- EnumDays-134Drill/EnumDays-134Drill/Program.cs
+++ EnumDays-134Drill/EnumDays-134Drill/Program.cs
@@ -10,6 +10,22 @@ namespace EnumDays_134Drill
     {
         static void Main(string[] args)
         {
+            //string[] daysString = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            Console.WriteLine("What is the day of the week?");
+            string input = Console.ReadLine();
+            try
+            {
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), input);
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+
+
+
         }
     }
 }
