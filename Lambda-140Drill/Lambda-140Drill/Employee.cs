0c682c18b0e3e619cb49d80ebcdd6e8d1870f9e6
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-13 19:59:40 -0800

    lambda 140

---
+++ Lambda-140Drill/Lambda-140Drill/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_140Drill
+{
+    public class Employee
+    {
+      public string fname { get; set; }
+        public string lname { get; set; }
+      public int id { get; set; }
+
+        public Employee(string firstName, string lastName, int ID)
+        {
+            fname = firstName;
+            lname = lastName;
+            id = ID;
+        }
+    }
+   
+}
---
+++ Lambda-140Drill/Lambda-140Drill/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_140Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            List<string> firstnames = new List<string>()
+            { "Ana", "Zachary",
+            "Joe","Joe",
+            "Emmy","Devin",
+            "Molly","Cody",
+            "Hilary","Erik",};
+
+            List<string> lastname = new List<string>()
+            { "Meyer","Leo",
+            "Orozco","Daniels",
+            "Kitts","Sawa",
+            "Good","Limbach",
+            "Browning","Miller",};
+
+            List<int> ids = new List<int>()
+            { 1,2,3,4,5,6,7,8,9,10};
+
+            int i = 0;
+            foreach (string FName in firstnames)
+            {
+                employees.Add(new Employee (FName, lastname[i], ids[i]));
+                i++;
+            }
+
+            List<Employee> joes = new List<Employee>();
+            int j = 0;
+            foreach (string Fname in firstnames)
+            {
+                if (firstnames =  "Joe")
+                {
+                    joes.Add(new Employee(Fname, lastname[j], ids[j]));
+                }
+
+            }
+
+            string joseph = employees.Where(x => x.fname == firstnames="Joe");
+
+            int bigId = ids.Where(y => y > 5);
+            Console.WriteLine(bigId);
+            Console.ReadLine();
+
+            }
+    }
+}
