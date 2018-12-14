673f78fc1784a9cdb9c5bc54871fc9bfde8e670b
Ana Meyer <ana.marie.meyer@gmail.com>
2018-12-13 17:05:42 -0800

    Struct drill 137

---
+++ Struct-137Drill/Struct-137Drill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_137Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            {
+                number.Amount = 100.50m;
+                Console.WriteLine(number.Amount);
+                Console.ReadLine();
+            }
+        }
+    }
+}
